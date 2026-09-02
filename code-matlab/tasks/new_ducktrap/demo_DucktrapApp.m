6e3ee3d34dfd8051a7f83034b6530207ceb61d98
lilytong0919 <106998357+lilytong0919@users.noreply.github.com>
2026-09-02 17:45:59 -0400

    Add Ducktrap app scaffold launcher

---
+++ code-matlab/tasks/new_ducktrap/demo_DucktrapApp.m
@@ -0,0 +1,9 @@
+function app = demo_DucktrapApp()
+%DEMO_DUCKTRAPAPP Open the non-functional Ducktrap app scaffold.
+%
+% Synthetic LFP, spike, event, and segment generation will be added in a
+% later development step.
+
+app = DucktrapApp();
+
+end
